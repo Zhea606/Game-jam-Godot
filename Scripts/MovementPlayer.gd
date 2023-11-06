63556bf5ef45b3247290795352ac3e507eaacca1
Cecilia Arias <ariascecilia606@gmail.com>
2023-11-06 13:48:40 -0300

    feat (need to fix) : add movement to the player
    the main scene doesn't execute when I press "Play"

--- Scripts/MovementPlayer.gd
+++ Scripts/MovementPlayer.gd
@@ -4,16 +4,16 @@ class_name Movement
 
 
 var speed: float = 32
-var max-speed: float = 32
+var max_speed: float = 32
 
 
 var character : CharacterBody2D
 
-func setup(character2D : CharacterBody2D)
+func setup(character2D : CharacterBody2D) :
 	character = character2D
 	
 	
-func move(input_vector: Vector2)
+func move(input_vector: Vector2) :
 	character.velocity = input_vector.normalized() * speed
 	character.move_and_slide()
 	
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,4 +2,21 @@ extends CharacterBody2D
 
 class_name Player
 
-@onready var 
+#permite obtener como shortcut, reduce código
+@onready var movement : Movement = $"Movement" as Movement 
+
+#almacena la informacion del input
+var input_vector: Vector2
+
+#inicializa el movimiento
+func _ready():
+	movement.setup(self) 
+
+#obtiene el input del player
+func _process(delta):
+	input_vector.x = Input.get_axis("move_left", "move_right")
+#	*********************************************************************falta el jump
+
+#funcion que mueve el personaje
+func _physics_process(delta):
+	movement.move(input_vector.normalized())
