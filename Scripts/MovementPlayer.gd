3ebbeb32f5b66581d1fc26236812528b2176fac1
Cecilia Arias <ariascecilia606@gmail.com>
2023-11-06 13:23:19 -0300

    feat: first commit to the repo

---
+++ Scripts/MovementPlayer.gd
@@ -0,0 +1,23 @@
+extends Node
+class_name Movement
+
+
+
+var speed: float = 32
+var max-speed: float = 32
+
+
+var character : CharacterBody2D
+
+func setup(character2D : CharacterBody2D)
+	character = character2D
+	
+	
+func move(input_vector: Vector2)
+	character.velocity = input_vector.normalized() * speed
+	character.move_and_slide()
+	
+func stop_movement() :
+	character.velocity = Vector2.ZERO
+	
+	
