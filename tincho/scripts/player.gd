010e49ab1e29298a3875a15d4eb5014d1b8db94f
Cecilia Arias <69162988+Zhea606@users.noreply.github.com>
2023-11-12 09:13:04 -0300

    fix: cache

--- tincho/scripts/player.gd
+++ tincho/scripts/player.gd
@@ -1,7 +1,6 @@
 extends CharacterBody2D
 
 const moveSpeed = 5
-const runSpeed =15  # Nueva velocidad de carrera
 const maxSpeed = 50
 
 const up = Vector2(0,-1)
@@ -13,45 +12,26 @@ const gravity = 15
 @onready var light = $PointLight2D
 @onready var notif = $notif
 
-
-var isRunning = false  # Nuevo flag para verificar si el personaje está corriendo
-
 func _physics_process(_delta):
 	velocity.y += gravity
 	var friction = false
 
-	if Input.is_action_pressed("right_move"):
+	if Input.is_action_pressed("ui_right"):
 		sprite.visible = false
 		spriteWalking.visible = true
 		sprite.flip_h = false
 		spriteWalking.flip_h = false
 		animationPlayer.play("Walk")
-		
-		if Input.is_action_pressed("sprint"):
-			velocity.x = min(velocity.x + runSpeed, maxSpeed)
-			isRunning = true
-		else:
-			velocity.x = min(velocity.x + moveSpeed, maxSpeed)
-			
-		
-#		velocity.x = min(velocity.x + moveSpeed, maxSpeed)
-#		light.rotation_degrees = 0
-	elif Input.is_action_pressed("left_move"):
+		velocity.x = min(velocity.x + moveSpeed, maxSpeed)
+		light.rotation_degrees = 0
+	elif Input.is_action_pressed("ui_left"):
 		sprite.visible = false
 		spriteWalking.visible = true
 		sprite.flip_h = true
 		spriteWalking.flip_h = true
 		animationPlayer.play("Walk")
-		
-		if Input.is_action_pressed("sprint"):
-			velocity.x = max(velocity.x - runSpeed, -maxSpeed)
-			isRunning = true
-			print("acelerando velocidad#")
-		else:
-			velocity.x = max(velocity.x - moveSpeed, -maxSpeed)
-#
-#		velocity.x = max(velocity.x - moveSpeed, -maxSpeed)
-#		light.rotation_degrees = 180
+		velocity.x = max(velocity.x - moveSpeed, -maxSpeed)
+		light.rotation_degrees = 180
 	elif Input.is_action_just_pressed("switch_light"):
 		if not light.enabled:
 			light.enabled = true
@@ -62,7 +42,6 @@ func _physics_process(_delta):
 		spriteWalking.visible = false
 		animationPlayer.play("Idle")
 		friction = true
-		isRunning=false
 
 	if is_on_floor():
 			
