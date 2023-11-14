f7b3788ec6c94a5057c88c02fd7fe0d4ce5a3e7c
Cecilia Arias <69162988+Zhea606@users.noreply.github.com>
2023-11-13 23:06:09 -0300

    bug: doesn't work the Tilemap

--- tincho/scripts/objeto1.gd
+++ tincho/scripts/objeto1.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 # Precargar la escena de dialogo
 #@export var dialogo_scene : PackedScene
-var dialogo = preload("res://dialogos/objects.dialogue")
+#var dialogo = preload("res://dialogos/objects.dialogue")
 var char_in_area = false
 
 func _on_body_entered(body):
@@ -21,7 +21,7 @@ func _on_body_exited(body):
 # Metodo para verificar eventos input en Areas 2D
 func _unhandled_input(event: InputEvent) -> void:
 	if char_in_area and Input.is_action_just_pressed("interact_object"):
-		DialogueManager.show_example_dialogue_balloon(dialogo,"start")
+#		DialogueManager.show_example_dialogue_balloon(dialogo,"start")
 		queue_free()
 
 func _on_ready():
