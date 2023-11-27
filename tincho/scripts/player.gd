60596e2693c3930a67ee1cb6dc2740bf53f2ed04
Cecilia Arias <69162988+Zhea606@users.noreply.github.com>
2023-11-27 00:18:23 -0300

    bug: added run animation, not working

--- tincho/scripts/player.gd
+++ tincho/scripts/player.gd
@@ -8,10 +8,12 @@ const gravity = 15
 
 @onready var sprite = $Sprite2D
 @onready var spriteWalking = $SpriteWalk
+@onready var spriteRunning = $SpriteRun
 @onready var animationPlayer = $AnimationPlayer
 @onready var light = $PointLight2D
 @onready var notif = $notif
 
+#usado para pausar el movimiento del personaje al aparecer un diálogo tutorial
 var is_paused = false
 var friction
 
@@ -22,17 +24,21 @@ func _physics_process(_delta):
 		if Input.is_action_pressed("right_move"):
 			sprite.visible = false
 			spriteWalking.visible = true
+			spriteRunning.visible = false
 			sprite.flip_h = false
 			spriteWalking.flip_h = false
 			animationPlayer.play("Walk")
-	#		velocity.x = min(velocity.x + moveSpeed, maxSpeed)
-	#
+#	        # //para sprint
 			if Input.is_action_pressed("sprint") :
-				velocity.x = min(velocity.x + moveSpeed, maxSpeed) * 5
+				velocity.x = min(velocity.x + moveSpeed, maxSpeed) * 3
+				sprite.visible = false
+				spriteWalking.visible = false
+				spriteRunning.visible = true
+				sprite.flip_h = false
+				spriteWalking.flip_h = false
+				animationPlayer.play("Run")
 			else :
-				velocity.x = min(velocity.x + moveSpeed, maxSpeed)
-
-			
+				velocity.x = min(velocity.x + moveSpeed, maxSpeed)			
 			light.rotation_degrees = 0
 		elif Input.is_action_pressed("left_move"):
 			sprite.visible = false
@@ -40,14 +46,11 @@ func _physics_process(_delta):
 			sprite.flip_h = true
 			spriteWalking.flip_h = true
 			animationPlayer.play("Walk")
-	#		velocity.x = max(velocity.x - moveSpeed, -maxSpeed)
-	#
+			# //para sprint
 			if Input.is_action_pressed("sprint") :
-				velocity.x = max(velocity.x - moveSpeed, -maxSpeed) * 5
+				velocity.x = max(velocity.x - moveSpeed, -maxSpeed) * 3
 			else :
 				velocity.x = max(velocity.x - moveSpeed, -maxSpeed)
-
-			
 			light.rotation_degrees = 180
 		elif Input.is_action_just_pressed("switch_light"):
 			if not light.enabled:
@@ -57,6 +60,7 @@ func _physics_process(_delta):
 		else:
 			sprite.visible = true
 			spriteWalking.visible = false
+			spriteRunning.visible = false			
 			animationPlayer.play("Idle")
 			friction = true
 
@@ -81,6 +85,7 @@ func stop_player() -> void:
 	if spriteWalking.visible:
 		sprite.visible = true
 		spriteWalking.visible = false
-	
+		spriteRunning.visible = false
+
 func resume_player() -> void:
 	is_paused = false
